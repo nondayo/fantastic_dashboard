c4b1e46e27597b42601e2b9b7e88c1cf91354768
LiChengEn <leslie.li@dsp.im>
2019-03-14 16:11:45 +0800

    create django branch

---
+++ mysite/dashboard/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+# a = {'name': 'Sarah', 'age': 24, 'isEmployed': True}
+# a python dictionary
+# def retjson():
+# python2json = json.dumps(a)
+# print python2json
+# retjson()
+
+
+def data():
+    a = {"error": [0],
+         "x": ["2019-02-19", "2019-02-18", "2019-02-17", "2019-02-16", "2019-02-15", "2019-02-14", "2019-02-13"],
+         "y": [1.52, 3.97, 5.61, 6.8, 9.48, 11.26, 9.33]}
+    return json.dumps(a)
+
+
+def mapdata():
+    m = [{"geometry": {"type": "Point", "coordinates": "[-104.9983545, 39.7502833]"}, "type": "Feature", "properties": {"popupContent": "popup 1"},
+          "id": 1}, {"geometry": {"type": "Point", "coordinates": "[-104, 39.76]"}, "type": "Feature", "properties": {"popupContent": "popup 2"}, "id": 2}]
+    return json.dumps(m)
